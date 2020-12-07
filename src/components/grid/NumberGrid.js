f4126b3053683be6722cd7db54805744e7c4a749
vikuviku6666 <53383503+vikuviku6666@users.noreply.github.com>
2020-12-07 13:28:25 +0100

    responsive fixed
    responsive fixed

--- src/components/grid/NumberGrid.js
+++ src/components/grid/NumberGrid.js
@@ -1,63 +1,84 @@
-import React from 'react';
-import {Grid} from 'react-virtualized';
+import React from "react";
+import {
+  Grid,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+} from "react-virtualized";
 import faker from "faker";
-import './NumberGrid.css';
+import "./NumberGrid.css";
 
-export default function NumberGrid(){
+export default function NumberGrid() {
+  const cache = React.useRef(
+    new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: 100,
+    })
+  );
   const [address, setAddress] = React.useState([]);
 
   React.useEffect(() => {
-  setAddress(
-    [...Array(1000000).keys()].map((key) => {
-      return {
-        id: key,
-        firstName: `${faker.name.firstName()}` ,
-        lastName: `${faker.name.lastName()}`,
-        account: `${faker.finance.account()}`,
-        accountName: `${faker.finance.accountName()}`,
-       companyName: `${faker.company.companyName()}`,
-        zipCode: `${faker.address.zipCode()}`,
-        city: `${faker.address.city()}`,
-        streetName: `${faker.address.streetName()}`,
-        streetAddress: `${faker.address.streetAddress()}`,
-        county: `${faker.address.county()}`,
-        country: `${faker.address.country()}`,
-        countryCode: `${faker.address.countryCode()}`,
-        state: `${faker.address.state()}`,
-        direction: `${faker.address.direction()}`,
-        timeZone: `${faker.address.timeZone()}`,
-      }
-    })
-  );
-  }, [])
-
-let list = address.map(function (obj) {
-  return Object.keys(obj).map(function (key) {
-    return obj[key];
-  });
-});
-  function cellRenderer({ columnIndex, key, rowIndex, style }) {
-    if (list.length > 0) { 
-    return (
-      <div key={key} style={style}>
-        { list[rowIndex][columnIndex]}
-      </div>
+    setAddress(
+      [...Array(10).keys()].map((key) => {
+        return {
+          id: key,
+          firstName: `${faker.name.firstName()}`,
+          lastName: `${faker.name.lastName()}`,
+          account: `${faker.finance.account()}`,
+          accountName: `${faker.finance.accountName()}`,
+          companyName: `${faker.company.companyName()}`,
+          zipCode: `${faker.address.zipCode()}`,
+          city: `${faker.address.city()}`,
+          streetName: `${faker.address.streetName()}`,
+          streetAddress: `${faker.address.streetAddress()}`,
+          county: `${faker.address.county()}`,
+          country: `${faker.address.country()}`,
+          countryCode: `${faker.address.countryCode()}`,
+          state: `${faker.address.state()}`,
+          direction: `${faker.address.direction()}`,
+          timeZone: `${faker.address.timeZone()}`,
+        };
+      })
     );
-  }
+  }, []);
+
+  let list = address.map(function (obj) {
+    return Object.keys(obj).map(function (key) {
+      return obj[key];
+    });
+  });
+  function cellRenderer({ columnIndex, key, rowIndex, style, parent }) {
+    if (list.length > 0) {
+      return (
+        <CellMeasurer
+          key={key}
+          cache={cache.current}
+          parent={parent}
+          columnIndex={columnIndex}
+          rowCount={rowIndex}
+        >
+          <div style={style}>{list[rowIndex][columnIndex]}</div>
+        </CellMeasurer>
+      );
+    }
   }
   console.log(list);
- 
+
   return (
-    <Grid
-      cellRenderer={cellRenderer}
-      columnCount={list.length > 0 && list[0].length}
-      columnWidth={100}
-      height={800}
-      rowCount={list.length > 0 && list.length}
-      rowHeight={100}
-      width={1000}
-    />
+    <div style={{ width: "100%", height: "100vh" }}>
+      <AutoSizer>
+        {({ width, height }) => (
+          <Grid
+            cellRenderer={cellRenderer}
+            columnCount={list.length > 0 && list[0].length}
+            columnWidth={cache.current.columnWidth}
+            height={height}
+            rowCount={list.length > 0 && list.length}
+            rowHeight={cache.current.rowHeight}
+            width={width}
+          />
+        )}
+      </AutoSizer>
+    </div>
   );
-  }
-             
-            
+}
